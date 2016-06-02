6f4998cc469e8ff512bb5984b61b97420724df89
ethan <ethanzhen@163.com>
2016-06-02 22:55:29 +0800

    第一次

--- app/src/main/java/com/pz/demo/MainActivity.java
+++ app/src/main/java/com/pz/demo/MainActivity.java
@@ -1,15 +1,29 @@
 package com.pz.demo;
 
+import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //测试commite
+        tv= (TextView) findViewById(R.id.tv);
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ObjectAnimator animator=ObjectAnimator.ofFloat(tv,"translationX",0,300);
+                animator.setDuration(10000);
+                animator.setStartDelay(5000);
+                animator.setCurrentPlayTime(5000);
+            }
+        });
+
+
 
     }
 }
