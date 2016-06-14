02d4582f9c344e1e3261007e61a3eb97d384a4c7
ethan <ethanzhen@163.com>
2016-06-14 08:19:32 +0800

    第一次

--- app/src/main/java/com/pz/demo/ProgressImageView.java
+++ app/src/main/java/com/pz/demo/ProgressImageView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.widget.ImageView;
@@ -46,13 +47,24 @@ public class ProgressImageView extends ImageView{
         can.rotate(-rotation,getWidth()/2,getHeight()/2);
         can.translate(0,2*width-2);
         can.drawRect(-xOffset,-yOffset,width+xOffset,width+yOffset,paint);
-
-
         invalidate();
     }
 
     int caculateMaskRectWidth(int width,int height){
         return (int) Math.sqrt(width*width+height*height);
     }
+
+    Point caculateTranslat(){
+        Point p=new Point();
+        float rotation=getRotation();
+        double radius=  Math.toRadians(rotation);
+        double r=  Math.sqrt(getWidth()*getWidth()+getHeight()*getHeight());
+        double radius90=Math.toRadians(90);
+        double radiusB=Math.atan(getHeight()/getWidth());
+        p.x= (int) (getWidth()*Math.cos(radius)+getHeight()*Math.sin(radius)+2*Math.sin(radius/2)*r*Math.sin(radius90+radius/2-radiusB));
+        p.y= (int) (getHeight()*Math.cos(radius)-getWidth()*Math.sin(radius)+2*Math.cos(radius/2)*r*Math.sin(radius90+radius/2-radiusB));
+        return p;
+    }
+
 
 }
